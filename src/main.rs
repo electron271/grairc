214d5c2922b017ae64ad7426f8ee6791819a86b0
electron271 <66094410+electron271@users.noreply.github.com>
2026-01-10 14:09:44 -0600

    init

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use ctru::prelude::*;
+
+fn main() {
+    let apt = Apt::new().unwrap();
+    let mut hid = Hid::new().unwrap();
+    let gfx = Gfx::new().unwrap();
+    let _console = Console::new(gfx.top_screen.borrow_mut());
+
+    println!("Hello, World!");
+    println!("\x1b[29;16HPress Start to exit");
+
+    while apt.main_loop() {
+        gfx.wait_for_vblank();
+
+        hid.scan_input();
+        if hid.keys_down().contains(KeyPad::START) {
+            break;
+        }
+    }
+}
